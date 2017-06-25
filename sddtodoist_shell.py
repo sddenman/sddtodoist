d7e81019b46a5aaef5ada8f2877e7516af6ff9d3
sddenman <sddenman@us.ibm.com>
2017-06-25 17:49:06 -0600

    fix_addlabel_issues

--- sddtodoist_shell.py
+++ sddtodoist_shell.py
@@ -15,10 +15,10 @@ def command_additem(command_args):
 
     if len(projects) == 0:
         write_result_and_exit(1, vars_args, None,
-                              parser.prog+"ERROR: Project with name "+vars_args['project']+" not found.")
+                              parser.prog+" ERROR: Project with name "+vars_args['project']+" not found.")
     elif len(projects) > 1:
         write_result_and_exit(1, vars_args, None,
-                              parser.prog+"ERROR: Multiple projects with name "+vars_args['project']+" found.")
+                              parser.prog+" ERROR: Multiple projects with name "+vars_args['project']+" found.")
     else:
         add_item_args = {'project_id': projects[0]['id'], 'title': vars_args['name']}
         for key in ['due_date', 'comment', 'labels', 'priority']:
@@ -30,26 +30,36 @@ def command_additem(command_args):
 
         new_item = add_item(**add_item_args)
 
-        write_result_and_exit(0, vars_args, new_item, parser.prog+" New item created with ID="+str(new_item['id'])+".")
+        write_result_and_exit(0, vars_args, new_item, parser.prog+": New item created with ID="+str(new_item['id'])+".")
 
 
 def command_addlabel(command_args):
 
     vars_args = vars(command_args)
 
-    # TODO Update command_addlabel to allow list of itemid's
-    if vars_args['itemid']:
-        new_label = add_label(vars_args['name'], vars_args['itemid'])
+    if vars_args['itemids']:
+        try:
+            new_label = add_label(vars_args['name'], vars_args['itemids'].split(","))
+        except AttributeError:
+            write_result_and_exit(1, vars_args, dict(),
+                                  parser.prog +
+                                  " ERROR: One or more of item ids " + str(vars_args['itemids']) + " not found."
+                                  )
+            raise
+        write_result_and_exit(0, vars_args, new_label,
+                              parser.prog +
+                              ": Label with name '" + str(new_label['name']) +
+                              "' and id " + str(new_label['id']) +
+                              " added to items with id(s) " + str(vars_args['itemids']) + "."
+                              )
     else:
         new_label = add_label(vars_args['name'])
-
-    write_result_and_exit(0, vars_args, new_label,
-                          parser.prog +
-                          " Label (name:" + str(new_label['name']) +
-                          ", id:" + str(new_label['itemid']) +
-                          ") added to item (id:" + str(vars_args['itemid']) +
-                          ")."
-                          )
+        write_result_and_exit(0, vars_args, new_label,
+                              parser.prog +
+                              ": Label with name '" + str(new_label['name']) +
+                              "' and id " + str(new_label['id']) +
+                              " added."
+                              )
 
 
 def command_addproject(command_args):
@@ -61,31 +71,22 @@ def command_addproject(command_args):
     if vars_args['parent']:
         project_objs = get_project_by_name(vars_args['project'])
         if len(project_objs) == 0:
-            write_result_and_exit(0, vars_args, ,
-                                  parser.prog +
-                                  " (name:" + str(new_label['name']) +
-                                  ", id:" + str(new_label['itemid']) +
-                                  ") added to item (id:" + str(vars_args['itemid']) +
-                                  ")."
-                                  )
-            print(parser.prog, "ERROR: Parent project", command_args.project, "not found.")
-            sys.exit(1)
+            write_result_and_exit(1, vars_args, dict(), parser.prog +
+                                  " ERROR: Parent project named " + str(vars_args['parent']) + " not found.")
         elif len(project_objs) > 1:
-            print(parser.prog, "ERROR: Multiple parent projects named", command_args.project, "found.")
-            sys.exit(1)
+            write_result_and_exit(1, vars_args, dict(), parser.prog +
+                                  " ERROR: Multiple parent projects named " + str(vars_args['parent']) + " found.")
         else:
             add_project_args['parent_project_id'] = project_objs[0]['id']
 
     new_project = add_project(**add_project_args)
 
-    if vars_args['pipeobj']:
-        sys.stdout.write(str(vars(new_project['project'])))
-    elif vars_args['pipeid']:
-        sys.stdout.write(str(new_project['project']['id']))
-    else:
-        print(parser.prog, "New project created with ID=", str(new_project['project']['id']))
-
-    sys.exit(0)
+    write_result_and_exit(0, vars_args, new_project,
+                          parser.prog +
+                          ": Project (name:" + str(new_project['name']) +
+                          ", id:" + str(new_project['itemid']) +
+                          ") added."
+                          )
 
 
 def command_removeproject(command_args):
@@ -100,7 +101,7 @@ def command_removeproject(command_args):
         action = "archived"
 
     write_result_and_exit(0, vars_args, removed_project,
-                          parser.prog + "Project with ID=" + str(removed_project['id']) +
+                          parser.prog + ": Project with ID=" + str(removed_project['id']) +
                           " and Name=" + removed_project['name'] + " " + action + "."
                           )
 
@@ -121,7 +122,7 @@ def command_addcomment(command_args):
     elif vars_args['pipeid']:
         sys.stdout.write(str(new_comment['id']))
     else:
-        print(parser.prog, "New comment created for item ID ", vars_args['itemid'], " with ID=", str(new_comment['id']))
+        print(parser.prog, ": New comment created for item ID ", vars_args['itemid'], " with ID=", str(new_comment['id']))
 
     sys.exit(0)
 
@@ -135,14 +136,15 @@ def command_getsyncresponse(command_args):
     if vars_args['key']:
         try:
             key_value = sync_response[vars_args['key']]
-            write_result_and_exit(0,
-                                  vars_args,
-                                  key_value,
-                                  vars_args['key'] + ": \n" +
-                                  str(pformat_todoist_obj(key_value))
-                                  )
         except:
-            write_result_and_exit(1, vars_args, None, parser.prog + " ERROR: " + vars_args['key'] + " key not found.")
+            write_result_and_exit(1, vars_args, None, parser.prog +
+                                  " ERROR: No key with name '" + vars_args['key'] + "' in sync response.")
+        write_result_and_exit(0,
+                              vars_args,
+                              key_value,
+                              vars_args['key'] + ": \n" +
+                              str(pformat_todoist_obj(key_value))
+                              )
     else:
         write_result_and_exit(0,
                               vars_args,
@@ -153,7 +155,7 @@ def command_getsyncresponse(command_args):
 
 def command_notimplemented(command_args):
 
-    print(parser.prog, "Command", command_args.command, "not yet implemented.")
+    print(parser.prog, ": Command", command_args.command, "not yet implemented.")
     sys.exit(1)
 
 
@@ -178,7 +180,7 @@ if __name__ == "__main__":
 
     parser_addlabel = subparsers.add_parser("addlabel", aliases=['al'], help="create label, optionally add to item(s)")
     parser_addlabel.add_argument("name", type=str, help="label name (will be created if it doesn't exist)")
-    parser_addlabel.add_argument("-i", "--itemids", type=int, help="Add label to this(these) item ID(s)")
+    parser_addlabel.add_argument("-i", "--itemids", type=str, help="Add label to item ID(s)")
     parser_addlabel.set_defaults(func=command_addlabel)
 
     parser_addcomment = subparsers.add_parser("addcomment", aliases=['ac'], help="add a comment to an item")
